a58e8fec2f92cad2e4ffa5069d3945910e6ff020
ManduSmiles <121136676+ManduSmiles@users.noreply.github.com>
2023-01-06 18:11:19 -0500

    adding in max and min forecast temps

--- src/index.js
+++ src/index.js
@@ -91,6 +91,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
+    console.log(forecastDay);
     if (index < 7 && index > 0) {
       forecastHTML =
         forecastHTML +
@@ -102,9 +103,15 @@ function displayForecast(response) {
                 <img class="forecast-icon" src="http://openweathermap.org/img/wn/${
                   forecastDay.weather[0].icon
                 }@2x.png" alt="" width="40"/>
-                <div class="weather-forecast-temperature">${Math.round(
+                <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">${Math.round(
                   forecastDay.temp.max
-                )}°</div>
+                )}° / </span>
+                <span class="weather-forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
+              
+                </div>
               </div>`;
     }
   });
