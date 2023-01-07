36dd20d604c3b6b49d3a713e940b5c21125a8ee5
ManduSmiles <121136676+ManduSmiles@users.noreply.github.com>
2023-01-07 11:24:08 -0500

    forecast display styles updated

--- src/index.js
+++ src/index.js
@@ -106,10 +106,10 @@ function displayForecast(response) {
                 <div class="weather-forecast-temperature">
                 <span class="weather-forecast-temperature-max">${Math.round(
                   forecastDay.temp.max
-                )}° / </span>
+                )}°    </span>
                 <span class="weather-forecast-temperature-min">${Math.round(
                   forecastDay.temp.min
-                )}°</span>
+                )}° </span>
               
                 </div>
               </div>`;
